675704531da882b9e1477d94f96b667a46bae7b4
Yurii Onufreiv <yurii.onufreiv@gmail.com>
2018-09-29 22:19:44 +0300

    add RainbowActivity

---
+++ app/src/main/java/onufreiv/cassiopeia/activity/RainbowActivity.kt
@@ -0,0 +1,27 @@
+package onufreiv.cassiopeia.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import onufreiv.cassiopeia.BluetoothHandler
+import onufreiv.cassiopeia.R
+
+class RainbowActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_rainbow)
+
+		findViewById<View>(R.id.smoothness_icr_button)
+				.setOnClickListener { BluetoothHandler.sendData("r") }
+
+		findViewById<View>(R.id.smoothness_dcr_button)
+				.setOnClickListener { BluetoothHandler.sendData("l") }
+
+		findViewById<View>(R.id.speed_icr_button)
+				.setOnClickListener { BluetoothHandler.sendData("u") }
+
+		findViewById<View>(R.id.speed_dcr_button)
+				.setOnClickListener { BluetoothHandler.sendData("d") }
+	}
+}
